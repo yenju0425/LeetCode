a6e641d636a287d1d90aa6f616e8cc3c1cb3232f
Rick <yenju0425@gmail.com>
2022-05-10 12:57:35 +0800

    05102022_Combination_Sum_III_60%

--- CPP/216. Combination Sum III/Combination_Sum_III.cpp
+++ CPP/216. Combination Sum III/Combination_Sum_III.cpp
@@ -5,14 +5,31 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        
+    vector<vector<int>> combinationSum3(int k, int n){
+        if(k == 0 and n == 0){
+            return vector<vector<int>>{{}};
+        }
+        if(k <= 0 or n <= 0){
+            return vector<vector<int>>{};
+        }
+        vector<vector<int>> comb;
+        for(int i = 1; i <= n / k; i++){
+            vector<vector<int>> postComb = combinationSum3(k - 1, n - i);
+            if(!postComb.empty()){
+                int postCombSize = postComb.size();
+                for(int j = 0; j < postCombSize; j++){
+                    postComb[j].push_back(i);
+                }
+                comb.insert(comb.end(), postComb.begin(), postComb.end());
+            }
+        }
+        return comb;
     }
 };
 
 int main(){
     Solution* S = new Solution();
-    vector<vector<int>> comb = S->combinationSum3(3, 9);
+    vector<vector<int>> comb = S->combinationSum3(4, 9);
     for(int i = 0; i < comb.size(); i++){
         for(int j = 0; j < comb[i].size(); j++){
             cout << comb[i][j] << ' ';
