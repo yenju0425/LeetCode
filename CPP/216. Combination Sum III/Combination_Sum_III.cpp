62b183af3e804b63ea6ce473baae2c9b4bbc06d7
Rick <yenju0425@gmail.com>
2022-05-10 14:40:59 +0800

    05102022_Combination_Sum_III_allpass

--- CPP/216. Combination Sum III/Combination_Sum_III.cpp
+++ CPP/216. Combination Sum III/Combination_Sum_III.cpp
@@ -6,6 +6,10 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n){
+        return combinationSum3(k, n, 1);
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n, int lowerBound){
         if(k == 0 and n == 0){
             return vector<vector<int>>{{}};
         }
@@ -13,8 +17,9 @@ public:
             return vector<vector<int>>{};
         }
         vector<vector<int>> comb;
-        for(int i = 1; i <= n / k; i++){
-            vector<vector<int>> postComb = combinationSum3(k - 1, n - i);
+        int upperbound = min(9, n / k);
+        for(int i = lowerBound; i <= upperbound; i++){
+            vector<vector<int>> postComb = combinationSum3(k - 1, n - i, i + 1);
             if(!postComb.empty()){
                 int postCombSize = postComb.size();
                 for(int j = 0; j < postCombSize; j++){
@@ -29,7 +34,7 @@ public:
 
 int main(){
     Solution* S = new Solution();
-    vector<vector<int>> comb = S->combinationSum3(4, 9);
+    vector<vector<int>> comb = S->combinationSum3(3, 9);
     for(int i = 0; i < comb.size(); i++){
         for(int j = 0; j < comb[i].size(); j++){
             cout << comb[i][j] << ' ';
