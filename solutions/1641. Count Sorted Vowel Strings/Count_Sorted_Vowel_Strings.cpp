ac1c4d940d66c5dbc2fafdc1479ef55fb36ef6e6
Rick <yenju0425@gmail.com>
2022-05-11 12:46:13 +0800

    20220510_count_sorted_vowel_string_allpass

--- solutions/1641. Count Sorted Vowel Strings/Count_Sorted_Vowel_Strings.cpp
+++ solutions/1641. Count Sorted Vowel Strings/Count_Sorted_Vowel_Strings.cpp
@@ -5,10 +5,12 @@ using namespace std;
 class Solution {
 public:
     int countVowelStrings(int n) {
-        
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
     }
 };
 
 int main(){
+    Solution* S = new Solution();
+    cout << S->countVowelStrings(33) << endl;
     return 0;
 }
