b1fcd75d419a0d3e76148887fe0cfc466492702d
Rick <yenju0425@gmail.com>
2022-05-12 17:44:31 +0800

    20220512 update 9

---
+++ Solutions/9. Palindrome Number/CPP/Palindrome_Number_withoutUsingString.cpp
@@ -0,0 +1,26 @@
+//Reference: https://leetcode.com/problems/palindrome-number/solution
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    bool isPalindrome(int x){
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        int reverted_x = 0;
+        while(x > reverted_x){
+            reverted_x = reverted_x * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == reverted_x || x == reverted_x/10; // ex: "12321" will cause x = 12, reverted_x = 123
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+    cout << S->isPalindrome(12321) << endl;
+    return 0;
+}
