5814dc2f9b5492eae70557ade5655fd074c79971
Rick <yenju0425@gmail.com>
2022-05-12 13:55:01 +0800

    20220512 47. Permutations II (100%) add_comments

--- Solutions/47. Permutations II/Permutations_II.cpp
+++ Solutions/47. Permutations II/Permutations_II.cpp
@@ -11,30 +11,35 @@ public:
         vector<vector<int>> P = {};
         int numList_size = numList.size();
         for(int i = 0; i < numList_size; i++){
-            if(numList[i].second > 0){
-                // get the first number
-                int prefix = numList[i].first;
+            // get the first number
+            int prefix = numList[i].first;
 
-                // get the permutation of the remaining numbers
-                numList[i].second = numList[i].second - 1;
-                vector<vector<int>> p = permutation(numList);
-                numList[i].second = numList[i].second + 1;
+            // get the permutation of the remaining numbers
+            vector<pair<int, int>> new_numList = numList;
+            new_numList[i].second = new_numList[i].second - 1;
+            if(new_numList[i].second == 0){
+                new_numList.erase(new_numList.begin() + i);
+            }
 
-                if(p.empty()){
-                    p.push_back(vector<int>());
-                }
-                int p_size = p.size();
-                for(int j = 0; j < p_size; j++){
-                    p[j].push_back(prefix);
-                    P.push_back(p[j]);
-                }
+            vector<vector<int>> p = permutation(new_numList);
+            if(p.empty()){
+                p = vector<vector<int>>{{}};
+            }
+
+            int p_size = p.size();
+            for(int j = 0; j < p_size; j++){
+                p[j].push_back(prefix);
+                P.push_back(p[j]);
             }
         }
         return P;
     }
+
     vector<vector<int>> permuteUnique(vector<int>& nums){
-        vector<pair<int, int>> numList;
         sort(nums.begin(), nums.end());
+
+        //create number list
+        vector<pair<int, int>> numList;
         int nums_size = nums.size();
         for(int i = 0; i < nums_size; i++){
             if(numList.empty() or numList.back().first != nums[i]){
@@ -50,7 +55,14 @@ public:
 
 int main(){
     Solution* S = new Solution();
-    vector<int>nums{1, 1, 3};
+    vector<int> nums{1, 1, 2, 2, 3};
     vector<vector<int>> result = S->permuteUnique(nums);
+    cout << "number of permutations: " << result.size() << endl;
+    for(auto i : result){
+        for(auto j : i){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
