e126f0ceadfb30eaa0048edd6096f8e0b1cad726
Rick <yenju0425@gmail.com>
2022-05-13 11:54:38 +0800

    20220513 117. Populating Next Right Pointers in Each Node II (0%)

---
+++ Solutions/117. Populating Next Right Pointers in Each Node II/CPP/Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int main(){
+    return 0;
+}
