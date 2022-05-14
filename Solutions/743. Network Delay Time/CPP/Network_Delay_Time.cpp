bf6f9991574d480d540d8aab07acb1a2f6429097
Rick <yenju0425@gmail.com>
2022-05-14 19:36:21 +0800

    20220514 743. Network Delay Time (40%)

--- Solutions/743. Network Delay Time/CPP/Network_Delay_Time.cpp
+++ Solutions/743. Network Delay Time/CPP/Network_Delay_Time.cpp
@@ -8,23 +8,52 @@ using namespace std;
 
 class Vertex{
 private:
-    map<int, int> adjList = {};
+    Vertex* parent;
+    int time2parent;
+    bool visited;
+    map<int, int> adjList;
 
 public:
+    Vertex(){
+        parent = nullptr;
+        time2parent = numeric_limits<int>::max();
+        visited = false;
+    }
+
+    //Utilities:
+    int time2source(){
+        return (this == parent) ? 0 : time2parent + parent->time2source();
+    }
+
+    //Element access:
     map<int, int> getAdjList(){
         return adjList;
     }
+    bool isVisited(){
+        return visited;
+    }
 
+    //Modifiers:
+    void setParent(Vertex* p, int t){
+        parent = p;
+        time2parent = t;
+    }
     void addEdge(int v, int t){
         adjList[v] = t;
+    }
+    void visit(){
+        visited = true;
     }
 };
 
 class Solution{
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k){
+    int networkDelayTime(vector<vector<int>>& times, int n, int k){ //n vertices, start from k
         map<int, Vertex*> vertices;
-        map<int, int> minTimes;
+        for(int i = 1; i <= n; i++){
+            vertices[i] = nullptr;
+        }
+        int numVisited = 0;
 
         //build graph
         for(vector<vector<int>>::iterator i = times.begin(); i != times.end(); i++){
@@ -34,35 +63,38 @@ public:
 
             if(vertices[v1] == nullptr){
                 vertices[v1] = new Vertex();
-                minTimes[v1] = numeric_limits<int>::max();
-            }
-            if(vertices[v2] == nullptr){
-                vertices[v2] = new Vertex();
-                minTimes[v2] = numeric_limits<int>::max();
             }
             vertices[v1]->addEdge(v2, t);
         }
 
-        //Dijkstra Algorithm
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> findMin; //findMin->first: time needed; findMin->second: go to where
-        findMin.push(vector<int>{0, k});
-
+        //Prim's Algorithm
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> findMin; //findMin[0]: time needed; findMin[1]: next vertex; findMin[2]: from which vertex; 
+        findMin.push(vector<int>{0, k, k});
         while(!findMin.empty()){
-            int nextT = findMin.top()[0];
-            int nextV = findMin.top()[1];
-            minTimes[nextV] = nextT;
+            int T = findMin.top()[0];
+            int V = findMin.top()[1];
+            int P = findMin.top()[2];
             findMin.pop();
 
-            if(nextV == n){ //target found
-                return nextT;
+            vertices[V]->visit();
+            vertices[V]->setParent(vertices[P], T);
+            numVisited = numVisited + 1;
+
+            if(numVisited == n){ //target found
+                return vertices[V]->time2source();
             }
 
             //update neighbors
-            map<int, int> neighbors = vertices[nextV]->getAdjList();
+            map<int, int> neighbors = vertices[V]->getAdjList();
             for(map<int, int>::iterator i = neighbors.begin(); i != neighbors.end(); i++){
                 int neighborV = i->first;
                 int neighborT = i->second;
-                findMin.push(vector<int>{neighborT + minTimes[nextV], neighborV});
+                if(vertices[neighborV] == nullptr){
+                    numVisited = numVisited + 1;
+                }
+                else if(vertices[neighborV]->isVisited() == false){
+                    findMin.push(vector<int>{neighborT, neighborV, V});
+                }
             }
         }
         return -1;
@@ -70,9 +102,9 @@ public:
 };
 
 int main(){
-    vector<vector<int>> G{{1,2,1}};
+    vector<vector<int>> G{{2,1,1},{2,3,1},{3,4,1}};
 
     Solution* S = new Solution();
-    cout << S->networkDelayTime(G, 2, 2) << endl;
+    cout << S->networkDelayTime(G, 4, 2) << endl;
     return 0;
 }
