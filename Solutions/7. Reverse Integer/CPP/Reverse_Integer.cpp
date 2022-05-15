e2ca5ecbdf414dc1b19c75ccb5bd03065d52c984
Rick <yenju0425@gmail.com>
2022-05-15 14:17:34 +0800

    20220515 7. Reverse Integer (100%)

--- Solutions/7. Reverse Integer/CPP/Reverse_Integer.cpp
+++ Solutions/7. Reverse Integer/CPP/Reverse_Integer.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
+class Solution{
+public:
+    bool addDigit(int &reversed_n, int &n){
+        if(reversed_n > 214748364 or reversed_n < -214748364){
+            return true;
+        }
+
+        int temp = reversed_n * 10;
+        reversed_n = temp + n % 10;
+        n = n / 10;
+        
+        if((reversed_n > 0 and temp < 0) or (reversed_n < 0 and temp > 0)){
+            return true;
+        }
+
+        return false; //no overflow detected
+    }
+
+    int reverse(int x){
+        int reversed_x = x % 10;
+        x = x / 10;
+
+        while(x != 0){
+            if(addDigit(reversed_x, x)){ //"true" if overflow detected
+                return 0;
+            }
+        }
+
+        return reversed_x;
+    }
+};
+
 int main(){
+    Solution* S = new Solution();
+    cout << S->reverse(-123) << endl;
     return 0;
 }
