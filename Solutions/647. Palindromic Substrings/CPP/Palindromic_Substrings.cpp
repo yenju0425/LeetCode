07e725c4a49b8f6c923932de368f78a8e9af5dd7
Rick <yenju0425@gmail.com>
2022-05-22 12:52:28 +0800

    647. Palindromic Substrings (100%)

---
+++ Solutions/647. Palindromic Substrings/CPP/Palindromic_Substrings.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int countSubstrings(string s){
+        int stringCounter = 0;
+        int stringLength = s.size();
+        for(float i = 0; i <= stringLength - 1; i = i + 0.5){
+            int l = i;
+            int r = i + 0.5;
+            while((l >= 0 and r < stringLength) and s[l] == s[r]){
+                stringCounter = stringCounter + 1;
+                l = l - 1;
+                r = r + 1;
+            }
+        }
+        return stringCounter;
+    }
+};
+
+int main(){
+    Solution* S  = new Solution();
+
+    cout << S->countSubstrings("abcaac") << endl;
+
+    return 0;
+}
