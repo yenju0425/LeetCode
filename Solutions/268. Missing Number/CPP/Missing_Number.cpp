6086127a59cdcdba49564877042763672901cc84
Rick <yenju0425@gmail.com>
2022-05-28 13:46:16 +0800

    268. Missing Number (100%) 23. Merge k Sorted Lists (100%)

--- Solutions/268. Missing Number/CPP/Missing_Number.cpp
+++ Solutions/268. Missing Number/CPP/Missing_Number.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-class Solution {
+class Solution{
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& nums){
         int range = nums.size(); //from 0 ~ range
 
         vector<bool> checker(range + 1, false);
