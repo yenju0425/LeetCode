cefb9a551983f2d53051f684f42dcf46e612d225
Rick <yenju0425@gmail.com>
2022-05-30 20:53:03 +0800

    20220530 25. Reverse Nodes in k-Group (100%)

--- Solutions/24. Swap Nodes in Pairs/CPP/Swap_Nodes_in_Pairs_optimized.cpp
+++ Solutions/24. Swap Nodes in Pairs/CPP/Swap_Nodes_in_Pairs_optimized.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <utility>
-#include <vector>
 
 using namespace std;
 
--- Solutions/25. Reverse Nodes in k-Group/CPP/Reverse_Nodes_in_k-Group.cpp
+++ Solutions/25. Reverse Nodes in k-Group/CPP/Reverse_Nodes_in_k-Group.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <utility>
-#include <vector>
-#include <queue>
 
 using namespace std;
 
