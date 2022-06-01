7fe5abf268eeebb5cab294caf38aa30a2227bd81
Rick <yenju0425@gmail.com>
2022-06-01 15:26:47 +0800

    20220601 53, 66 (100%)

---
+++ Solutions/53. Maximum Subarray/CPP/Maximum_Subarray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution{
+public:
+    int maxSubArray(vector<int> &nums){
+        int sum = 0;
+        int maxSum = INT_MIN;
+        
+        for(vector<int>::iterator i = nums.begin(); i != nums.end(); i++){
+            sum = sum + *i;
+            maxSum = max(maxSum, sum);
+            sum = max(sum, 0); //re-accumulate sum
+        }
+
+        return maxSum;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    vector<int> nums{-2};
+
+    cout << S->maxSubArray(nums) << endl;
+
+    return 0;
+}
---
+++ Solutions/66. Plus One/CPP/Plus_One.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    vector<int> plusOne(vector<int> &digits){
+        int digits_size = digits.size();
+        for(int i = digits_size - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    vector<int> nums{9, 9, 9};
+
+    vector<int> result = S->plusOne(nums);
+
+    for(auto i : result){
+        cout << i << ' ';
+    }
+
+    return 0;
+}
