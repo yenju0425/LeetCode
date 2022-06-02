bce151c5302a4c929c1e6523ee5a63289d35e10a
Rick <yenju0425@gmail.com>
2022-06-02 16:48:42 +0800

    20220602 69 (100%)

---
+++ Solutions/69. Sqrt(x)/CPP/69. Sqrt(x).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int mySqrt(int x){
+        long i = 1;
+        while(i * i <= x){
+            i = i + 1;
+        }
+        return i - 1;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    int x = 8;
+
+    cout << S->mySqrt(x) << endl;
+
+    return 0;
+}
---
+++ Solutions/69. Sqrt(x)/CPP/69. Sqrt(x)_optimized.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int mySqrt(int x){
+        long i = 0;
+
+        //slow start method
+        int step = 1;
+        while(true){
+            i = i + step;
+            if(i * i <= x){
+                step = max(step + 1, 16);
+            }
+            else{
+                if(step != 1){
+                    i = i - step;
+                    step = 1;
+                }
+                else{
+                    return i - 1;
+                }
+            }
+        }
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    int x = 0;
+
+    cout << S->mySqrt(x) << endl;
+
+    return 0;
+}
