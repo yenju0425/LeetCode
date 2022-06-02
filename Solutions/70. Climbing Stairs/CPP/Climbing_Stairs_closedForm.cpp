bf8ec172b7e5e90663f5d0efdf7bc10b4102a68c
Rick <yenju0425@gmail.com>
2022-06-02 22:17:19 +0800

    20220602 update RM

--- Solutions/70. Climbing Stairs/CPP/Climbing_Stairs_closedForm.cpp
+++ Solutions/70. Climbing Stairs/CPP/Climbing_Stairs_closedForm.cpp
@@ -8,7 +8,7 @@ public:
     int climbStairs(int n){
         n = n + 1; //in order to apply the Fibonacci closed-form expression
         double x = sqrt(5);
-        return int((1 / x) * (pow((1 + x)/2, n) - pow((1 - x)/2, n)) + 0.5);
+        return int((1 / x) * (pow((1 + x)/2, n) - pow((1 - x)/2, n)) + 0.5); //0.5: round double
     }
 };
 
