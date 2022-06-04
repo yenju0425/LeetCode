b009455e16dd7168b4f56dbdec213705c4e92d07
Rick <yenju0425@gmail.com>
2022-06-04 11:22:09 +0800

    20220604 37. Sudoku Solver (100%)

--- Solutions/37. Sudoku Solver/CPP/Sudoku_Solver.cpp
+++ Solutions/37. Sudoku Solver/CPP/Sudoku_Solver.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 class Solution{
 private:
-    vector<vector<vector<bool>>> findNum_row; //findNum_row[i][j] stores all the possible numbers that can be filled in "board"
-    vector<vector<vector<bool>>> findNum_col;
-    vector<vector<vector<bool>>> findNum_blk;
-
     int counter;
     bool stuck;
+    vector<vector<vector<bool>>> availableNum_r; //availableNum_r[i][j] stores all the available numbers that can be filled in "i-th" row's "j-th" position
+    vector<vector<vector<bool>>> availableNum_c; //availableNum_c[i][j] stores all the available numbers that can be filled in "i-th" col's "j-th" position
+    vector<vector<vector<bool>>> availableNum_b; //availableNum_b[i][j] stores all the available numbers that can be filled in "i-th" blk's "j-th" position
 
 public:
     int blockId(int &i, int &j){
@@ -25,95 +24,63 @@ public:
         return (counter == 81) ? true : false;
     }
 
-    void updateFindNum(int row, int col, int num){ //claim that board[i][j] will be fill with "num"
+    bool canFill(int row, int col, int num){
+        int blk = blockId(row, col);
+        int idx = indexInBlock(row, col);
+        return availableNum_r[row][col][num] and availableNum_c[col][row][num] and availableNum_b[blk][idx][num];
+    }
+
+    void updateAvailableNum(int row, int col, int num){
         int blk = blockId(row, col);
         int idx = indexInBlock(row, col);
 
-        fill(findNum_row[row][col].begin(), findNum_row[row][col].end(), 0); //the "col-th" position for other numbers in the "row-th" row is unavailable (each position can only be filled with one number)
-        fill(findNum_col[col][row].begin(), findNum_col[col][row].end(), 0);
-        fill(findNum_blk[blk][idx].begin(), findNum_blk[blk][idx].end(), 0);
+        fill(availableNum_r[row][col].begin(), availableNum_r[row][col].end(), false); //the "col-th" position for other numbers in the "row-th" row is unavailable (each position can only be filled with one number)
+        fill(availableNum_c[col][row].begin(), availableNum_c[col][row].end(), false);
+        fill(availableNum_b[blk][idx].begin(), availableNum_b[blk][idx].end(), false);
 
         for(int k = 0; k < 9; k++){
-            findNum_row[row][k][num] = false; //every position in the "row-th" row cannot be filled with "num" (each number can only be used once)
-            findNum_col[col][k][num] = false;
-            findNum_blk[blk][k][num] = false;
+            availableNum_r[row][k][num] = false; //every position in the "row-th" row cannot be filled with "num" (each number can only be used once)
+            availableNum_c[col][k][num] = false;
+            availableNum_b[blk][k][num] = false;
         }
     }
 
     void fillNum(int row, int col, int num, vector<vector<char>> &board){
-        updateFindNum(row, col, num);
+        stuck = false;
         if(board[row][col] == '.'){
             board[row][col] = num + '1';
             counter = counter + 1;
+            updateAvailableNum(row, col, num);
         }
     }
 
     void solveSudoku(vector<vector<char>> &board){
-        //init findNum_row, findNum_col, findNum_blk
-        findNum_row = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
-        findNum_col = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
-        findNum_blk = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
-
-        //init counter. If counter == 81, problem is solved!
+        //init counter
         counter = 0;
-        stuck   = true;
 
-        //pre-load batch for the first update
+        //init availableNum_r, availableNum_c, availableNum_b
+        availableNum_r = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
+        availableNum_c = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
+        availableNum_b = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 if(board[i][j] != '.'){
                     counter = counter + 1;
-                    updateFindNum(i, j, board[i][j] - '1');
-                    stuck = false;
+                    updateAvailableNum(i, j, board[i][j] - '1');
                 }
             }
         }
 
-        while(true){
-            if(isEnd()){
-                return;
-            }
-            if(stuck){ //iteratively guess an answer for the next empty cell
-                for(int i = 0; i < 9; i++){
-                    for(int j = 0; j < 9; j++){
-                        for(int k = 0; k < 9; k++){
-                            int blk = blockId(i, j);
-                            int idx = indexInBlock(i, j);
-                            if(findNum_row[i][j][k] and findNum_col[j][i][k] and findNum_blk[blk][idx][k]){
-                                vector<vector<char>> board_clone = board;
-
-                                // guess k is the answer for board[i][j]
-                                board_clone[i][j] = k + '1';
-
-                                Solution* S = new Solution();
-                                S->solveSudoku(board_clone);
-
-                                if(board_clone[0][0] != '!'){ //if the guess is correct
-                                    board = board_clone;
-                                    return;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                board = vector<vector<char>>(9, vector<char>(9, '!')); //ERROR
-                return;
-            }
-
-            //get ready to fill in numbers for the next round
+        while(!isEnd()){
+            //reset stuck
             stuck = true;
             for(int i = 0; i < 9; i++){
                 for(int j = 0; j < 9; j++){
-                    //check by position: if board[i][j] can only be filled with k, than add (i, j, k) to batch
+                    //check by position:
                     if(board[i][j] == '.'){
-                        int blk = blockId(i, j);
-                        int idx = indexInBlock(i, j);
-
-                        //check whether k is the only number that can be filled in board[i][j]
                         int num = -1; //-1: null or collision
-                        for(int k = 0; k < 9; k++){
-                            if(findNum_row[i][j][k] and findNum_col[j][i][k] and findNum_blk[blk][idx][k]){
+                        for(int k = 0; k < 9; k++){ //check whether k is the only number that can be filled in board[i][j]
+                            if(canFill(i, j, k)){
                                 if(num == -1){
                                     num = k;
                                 }
@@ -126,27 +93,17 @@ public:
 
                         if(num >= 0){
                             fillNum(i, j, num, board);
-                            stuck = false;
                         }
                     }
 
-                    //note: In order to speed up, I combine "check by position" and "check by number" in the same loop. 'i', 'j', and 'k' have different meanings between these two parts 
-                    //check by number: if j can only be filled in board[i][k], than add (i, k, j) to batch
+                    //note: In order to speed up, I combine "check by position" and "check by number" in the same loop. 'i', 'j', and 'k' have different meanings between these two parts
+                    //check by number:
                     int pos_row = -1; //-1: null; -2: collision
                     int pos_col = -1;
                     int pos_blk = -1;
                     for(int k = 0; k < 9; k++){
-                        int row = -1;
-                        int col = -1;
-                        int blk = -1;
-                        int idx = -1;
-
                         //check whether j can only be place in the "k-th" position in "i-th" row
-                        row = i;
-                        col = k;
-                        blk = blockId(row, col);
-                        idx = indexInBlock(row, col);
-                        if(findNum_row[row][col][j] and findNum_col[col][row][j] and findNum_blk[blk][idx][j]){
+                        if(canFill(i, k, j)){
                             if(pos_row == -1){
                                 pos_row = k;
                             }
@@ -156,11 +113,7 @@ public:
                         }
 
                         //check whether j can only be place in the "k-th" position in "i-th" col
-                        row = k;
-                        col = i;
-                        blk = blockId(row, col);
-                        idx = indexInBlock(row, col);
-                        if(findNum_row[row][col][j] and findNum_col[col][row][j] and findNum_blk[blk][idx][j]){
+                        if(canFill(k, i, j)){
                             if(pos_col == -1){
                                 pos_col = k;
                             }
@@ -170,11 +123,7 @@ public:
                         }
                         
                         //check whether j can only be place in the "k-th" position in "i-th" blk
-                        row = (i / 3) * 3 + k / 3;
-                        col = (i % 3) * 3 + k % 3;
-                        blk = blockId(row, col);
-                        idx = indexInBlock(row, col);
-                        if(findNum_row[row][col][j] and findNum_col[col][row][j] and findNum_blk[blk][idx][j]){
+                        if(canFill((i / 3) * 3 + k / 3, (i % 3) * 3 + k % 3, j)){
                             if(pos_blk == -1){
                                 pos_blk = k;
                             }
@@ -197,7 +146,35 @@ public:
                     }
                 }
             }
+
+            if(stuck){ //iteratively guess an answer for the next empty cell
+                for(int i = 0; i < 9; i++){
+                    for(int j = 0; j < 9; j++){
+                        for(int k = 0; k < 9; k++){
+                            if(canFill(i, j, k)){
+                                vector<vector<char>> board_clone = board;
+
+                                // guess k is the answer for board[i][j]
+                                board_clone[i][j] = k + '1';
+
+                                Solution* S = new Solution();
+                                S->solveSudoku(board_clone);
+
+                                if(board_clone[0][0] != '!'){ //if the guess is correct
+                                    board = board_clone;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                board[0][0] = '!'; //ERROR
+                return;
+            }
         }
+
+        return;
     }
 };
 
