08975ca6cff471986514719a800691713ebf45c9
Rick <yenju0425@gmail.com>
2022-06-05 12:15:10 +0800

    20220605 125

---
+++ Solutions/125. Valid Palindrome/CPP/Valid_Palindrome.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution{
+public:
+    bool isPalindrome(string s){
+        //transform s to lower case
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        //push alphabets and numbers foward
+        string::iterator lastChar = remove_if(s.begin(), s.end(), [](char const &c){
+            return !std::isalnum(c);
+        });
+
+        //palindrome checker
+        for(string::iterator l = s.begin(), r = lastChar - 1; l <= r; l++, r--){
+            if(*l != *r){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    string s = " ";
+
+    cout << S->isPalindrome(s) << endl;
+
+    return 0;
+}
