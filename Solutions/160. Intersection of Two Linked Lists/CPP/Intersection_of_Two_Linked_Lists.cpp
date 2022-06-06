8f4654d43263def9d5b1204c5a56d512747801e5
Rick <yenju0425@gmail.com>
2022-06-06 12:34:23 +0800

    20220606 36, 160

---
+++ Solutions/160. Intersection of Two Linked Lists/CPP/Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr){}
+    ListNode(int x) : val(x), next(nullptr){}
+    ListNode(int x, ListNode* next) : val(x), next(next){}
+};
+
+class Solution{
+public:
+    ListNode* getIntersectionNode(ListNode *headA, ListNode *headB){
+        ListNode *Intersection;
+
+        ListNode *ptr;
+        for(ListNode *ptr = headA; ptr != nullptr; ptr = ptr->next){
+            ptr->val = (-1) * ptr->val;
+        }
+        for(ListNode *ptr = headB; ptr != nullptr; ptr = ptr->next){
+            if(ptr->val < 0){
+                Intersection = ptr;
+                break;
+            }
+        }
+
+        //restore listA
+        for(ListNode *ptr = headA; ptr != nullptr; ptr = ptr->next){
+            ptr->val = (-1) * ptr->val;
+        }
+
+        return Intersection;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    ListNode* c2 = new ListNode(5);
+    ListNode* c1 = new ListNode(4, c2);
+    ListNode* c0 = new ListNode(8, c1);
+
+    ListNode* a1 = new ListNode(1, c0);
+    ListNode* a0 = new ListNode(4, a1);
+    
+    ListNode* b2 = new ListNode(1, c0);
+    ListNode* b1 = new ListNode(6, b2);
+    ListNode* b0 = new ListNode(5, b1);
+
+    cout << S->getIntersectionNode(a0, b0)->val << endl;
+
+    return 0;
+}
---
+++ Solutions/36. Valid Sudoku/CPP/Valid_Sudoku.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+private:
+    vector<vector<vector<bool>>> availableNum_r; //availableNum_r[i][j] stores all the available numbers that can be filled in "i-th" row's "j-th" position
+    vector<vector<vector<bool>>> availableNum_c; //availableNum_c[i][j] stores all the available numbers that can be filled in "i-th" col's "j-th" position
+    vector<vector<vector<bool>>> availableNum_b; //availableNum_b[i][j] stores all the available numbers that can be filled in "i-th" blk's "j-th" position
+
+public:
+    int blockId(int &i, int &j){
+        return (i / 3) * 3 + (j / 3);
+    }
+
+    int indexInBlock(int &i, int &j){
+        return (i % 3) * 3 + (j % 3);
+    }
+
+    bool canFill(int row, int col, int num){
+        int blk = blockId(row, col);
+        int idx = indexInBlock(row, col);
+        return availableNum_r[row][col][num] and availableNum_c[col][row][num] and availableNum_b[blk][idx][num];
+    }
+
+    void updateAvailableNum(int row, int col, int num){
+        int blk = blockId(row, col);
+        int idx = indexInBlock(row, col);
+
+        fill(availableNum_r[row][col].begin(), availableNum_r[row][col].end(), false); //the "col-th" position for other numbers in the "row-th" row is unavailable (each position can only be filled with one number)
+        fill(availableNum_c[col][row].begin(), availableNum_c[col][row].end(), false);
+        fill(availableNum_b[blk][idx].begin(), availableNum_b[blk][idx].end(), false);
+
+        for(int k = 0; k < 9; k++){
+            availableNum_r[row][k][num] = false; //every position in the "row-th" row cannot be filled with "num" (each number can only be used once)
+            availableNum_c[col][k][num] = false;
+            availableNum_b[blk][k][num] = false;
+        }
+    }
+
+    bool isValidSudoku(vector<vector<char>> &board){
+        //init availableNum_r, availableNum_c, availableNum_b
+        availableNum_r = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
+        availableNum_c = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
+        availableNum_b = vector<vector<vector<bool>>>(9, vector<vector<bool>>(9, vector<bool>(9, true)));
+
+        //fill in the board one by one
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                //slip empty cells
+                if(board[i][j] == '.'){
+                    continue;
+                }
+
+                int num = (board[i][j] - '1');
+                if(canFill(i, j, num)){
+                    updateAvailableNum(i, j, num);
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    
+        return true;
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    vector<vector<char>> board{
+        {'8', '3', '.', '.', '7', '.', '.', '.', '.'}, 
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, 
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, 
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, 
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, 
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, 
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, 
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, 
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    cout << S->isValidSudoku(board) << endl;
+
+    return 0;
+}
