1a04cf5c9cf09eb09887d050d04f0a6ed40d6a4a
Rick <yenju0425@gmail.com>
2022-06-08 22:57:18 +0800

    20220608 update RM

---
+++ Solutions/50. Pow(x_n)/CPP/Pow(x_n).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n > 0){
+            double temp = 1;
+            while(n){
+                if(n & 1 == 1){
+                    temp = temp * x;
+                }
+                x = x * x;
+                n = n >> 1;
+            }
+            return temp;
+        }
+        else if(n < 0){
+            if(x == 0){
+                return 0;
+            }
+            double temp = 1;
+            long ln = -long(n);
+            while(ln){
+                if(ln & 1 == 1){
+                    temp = temp * x;
+                }
+                x  = x  *  x;
+                ln = ln >> 1;
+            }
+            return double(1 / temp);
+        }
+        
+        else{ //n == 0
+            return 1;
+        }
+    }
+};
+
+int main(){
+    Solution* S = new Solution();
+
+    //inputs
+    double x = 5;
+    int n = -2;
+
+    cout << S->myPow(x, n) << endl;
+
+    return 0;
+}
