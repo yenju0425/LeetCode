eb3d319f313a9bf21a6c7732a7e60800cdf2e1a8
Rick <yenju0425@gmail.com>
2022-06-14 18:10:55 +0800

    20220614 update 2 (Python)

--- Solutions/2. Add Two Numbers/Python/Add_Two_Numbers.py
+++ Solutions/2. Add Two Numbers/Python/Add_Two_Numbers.py
@@ -5,22 +5,21 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        l1_val = 0 if l1 == None else l1.val
-        l2_val = 0 if l2 == None else l2.val
+        v1 = 0 if l1 == None else l1.val
+        v2 = 0 if l2 == None else l2.val
 
-        l1_next = None if l1 == None else l1.next
-        l2_next = None if l2 == None else l2.next
+        l1 = l1.next if l1 != None else l1
+        l2 = l2.next if l2 != None else l2
 
-        div, mod = divmod(l1_val + l2_val, 10)
+        div, mod = divmod(v1 + v2, 10)
         node = ListNode(mod)
-        if div != 0 or l1_next != None or l2_next != None:
-            if l1_next == None:
-                newNode = ListNode(div)
-                l1_next = newNode
+        if div != 0 or l1 != None or l2 != None:
+            if l1 == None:
+                l1 = ListNode(div)
             else:
-                l1_next.val = l1_next.val + div
+                l1.val = l1.val + div
 
-            node.next = self.addTwoNumbers(l1_next, l2_next)
+            node.next = self.addTwoNumbers(l1, l2)
 
         return node
 
