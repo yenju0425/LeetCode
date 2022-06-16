46a1a85e4540af84ac4c1b8642d9dc0282e7442f
Rick <yenju0425@gmail.com>
2022-06-16 22:45:48 +0800

    20220616 75

---
+++ Solutions/75. Sort Colors/CPP/Sort_Colors.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    void swap(int &a, int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void sortColors(vector<int>& nums){
+        int nums_size = nums.size();
+    
+        int left  = 0;
+        int right = nums_size - 1;
+
+        int idx = 0;
+        while(idx <= right){
+            if(idx < left or nums[idx] == 1){
+                idx = idx + 1;
+            }
+            else if(nums[idx] == 0){
+                swap(nums[idx], nums[left]);
+                left = left + 1;
+            }
+            else{
+                swap(nums[idx], nums[right]);
+                right = right - 1;
+            }
+        }
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //input
+    vector<int> nums{2,0,2,1,1,0};
+
+    S->sortColors(nums);
+
+    for(auto i : nums){
+        cout << i << ' ';
+    }
+
+    return 0;
+}
