c3d225091b363ac00a287e0d78500071827437e0
Rick <yenju0425@gmail.com>
2022-06-18 11:48:13 +0800

    20220618 240, 968

---
+++ Solutions/240. Search a 2D Matrix II/CPP/Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    bool BS(int r_begin, int r_end, int c_begin, int c_end, vector<vector<int>> &M, int &T){
+        if(r_begin == r_end or c_begin == c_end){
+            return false;
+        }
+
+        int row = (r_begin + r_end) >> 1;
+        int col = (c_begin + c_end) >> 1;
+    
+        if(M[row][col] < T){
+            return BS( row + 1,   r_end, col + 1,   c_end, M, T) or
+                   BS( row + 1,   r_end, c_begin, col + 1, M, T) or
+                   BS( r_begin, row + 1, col + 1,   c_end, M, T);
+        }
+        else if(M[row][col] > T){
+            return BS( r_begin,     row, c_begin,     col, M, T) or
+                   BS( r_begin,     row, col    ,   c_end, M, T) or
+                   BS(     row,   r_end, c_begin,     col, M, T);
+        }
+        else{
+            return true;
+        }
+    }
+
+    bool searchMatrix(vector<vector<int>> &matrix, int &target){
+        return BS(0, matrix.size(), 0, matrix[0].size(), matrix, target);
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //inputs
+    int target = 7;
+    vector<vector<int>> matrix{
+        { 1,  4,  7}
+    };
+
+    cout << S->searchMatrix(matrix, target) << endl;
+
+    return 0;
+}
---
+++ Solutions/968. Binary Tree Cameras/CPP/Binary_Tree_Cameras.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+class Solution{
+public:
+    int* DFS(TreeNode *root){
+        //
+        //    result[0]: num of bulbs to light up the whole tree while using the root
+        //    result[1]: num of bulbs to light up the whole tree without using the root
+        //    result[2]: will light up the whole tree if we put a light at root's parent node
+        //
+        int *result = new int[3]{0};
+    
+        if(root == nullptr){
+            return result;
+        }
+
+        int *left  = DFS(root->left);
+        int *right = DFS(root->right);
+
+        //set result[0]
+        result[0] = min(left[0], min(left[1], left[2])) + min(right[0], min(right[1], right[2])) + 1;
+
+        //set result[1]
+        int  leftChild = ( left[0] == 0) ? INT_MAX :  left[0] + min(right[0], right[1]);
+        int rightChild = (right[0] == 0) ? INT_MAX : right[0] + min( left[0],  left[1]);
+        result[1] = min(leftChild, rightChild);
+
+        //set result[2]
+        result[2] = (left[1] == INT_MAX or right[1] == INT_MAX) ? INT_MAX : left[1] + right[1];
+
+        return result;
+    }
+
+    int minCameraCover(TreeNode *root){
+        int *result = DFS(root);
+        return min(result[0], result[1]);
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //input tree structure:
+    //
+    //    rt
+    //    |  \
+    //    n1  n2
+    //    |   |  \
+    //    n3  n4  n5
+    //
+    TreeNode *n5 = new TreeNode(5);
+    TreeNode *n4 = new TreeNode(4);
+    TreeNode *n3 = new TreeNode(3);
+    TreeNode *n2 = new TreeNode(2, n4, n5);
+    TreeNode *n1 = new TreeNode(1, n3, nullptr);
+    TreeNode *rt = new TreeNode(0, n1, n2);
+
+    cout << S->minCameraCover(rt) << endl;
+
+    return 0;
+}
