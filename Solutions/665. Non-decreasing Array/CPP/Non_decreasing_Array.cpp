269fa4a03c416a0d22ccf7082fab5f425ee2bae7
Rick <yenju0425@gmail.com>
2022-06-25 15:59:46 +0800

    20220625 665

---
+++ Solutions/665. Non-decreasing Array/CPP/Non_decreasing_Array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    bool checkPossibility(vector<int> &nums){
+        int nums_size = nums.size();
+    
+        int errorCounter = 0;
+        for(int i = 2; i < nums_size; i++){
+            if(nums[i - 2] > nums[i - 1]){
+                errorCounter = errorCounter + 1;
+            }
+            if(nums[i - 1] > nums[i]){
+                errorCounter = errorCounter + 1;
+                if(nums[i] >= nums[i - 2]){
+                    nums[i - 1] = nums[i];
+                }
+                else{
+                    nums[i] = nums[i - 1];
+                }
+            }
+
+            if(errorCounter == 2){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //input
+    vector<int> nums{5, 4, 3};
+
+    cout << S->checkPossibility(nums) << endl;
+
+    return 0;
+}
