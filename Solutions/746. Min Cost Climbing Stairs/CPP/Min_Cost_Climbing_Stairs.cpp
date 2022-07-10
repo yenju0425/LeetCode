343c7922d61f7932560fe17cc003571788301e34
Rick <yenju0425@gmail.com>
2022-07-10 22:12:39 +0800

    20220710 746

---
+++ Solutions/746. Min Cost Climbing Stairs/CPP/Min_Cost_Climbing_Stairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int minCostClimbingStairs(vector<int> &cost){
+        int cost_size = cost.size();
+        for(int i = 2; i < cost_size; i++){
+            cost[i] = cost[i] + min(cost[i - 1], cost[i - 2]);
+        }
+
+        return min(cost[cost_size - 1], cost[cost_size - 2]);
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //input
+    vector<int> cost{10, 15, 20};
+
+    cout << S->minCostClimbingStairs(cost) << endl;
+
+    return 0;
+}
---
+++ Solutions/746. Min Cost Climbing Stairs/CPP/Min_Cost_Climbing_Stairs_optimized.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int minCostClimbingStairs(vector<int> &cost){
+        int idx = 0;
+        int temp[2] = {cost[0], cost[1]};
+        for(vector<int>::iterator iter = cost.begin() + 2; iter != cost.end(); iter++){
+            temp[idx] = min(temp[0], temp[1]) + *iter;
+            idx = !idx;
+        }
+
+        return min(temp[0], temp[1]);
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //input
+    vector<int> cost{10, 15, 20};
+
+    cout << S->minCostClimbingStairs(cost) << endl;
+
+    return 0;
+}
