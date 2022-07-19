10b7787307c2bf8bb0e6abfa5546406cf2b14a02
Rick <yenju0425@gmail.com>
2022-07-19 13:02:50 +0800

    20220719 560, 629, 1074

---
+++ Solutions/560. Subarray Sum Equals K/CPP/Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution{
+public:
+    int subarraySum(vector<int> &nums, int k){
+        int result = (nums[0] == k);
+        unordered_map<int, int> UM{{0, 1}};
+        UM[nums[0]] = UM[nums[0]] + 1;
+
+        int nums_size = nums.size();
+        for(int i = 1; i < nums_size; i++){
+            nums[i]     = nums[i]     + nums[i - 1];
+            result      = result      + UM[nums[i] - k];
+            UM[nums[i]] = UM[nums[i]] + 1;
+        }
+
+        return result;
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //inputs
+    int k = 2;
+    vector<int> nums{1, 1, 1};
+
+    cout << S->subarraySum(nums, k) << endl;
+
+    return 0;
+}
---
+++ Solutions/629. K Inverse Pairs Array/CPP/K_Inverse_Pairs_Array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int kInversePairs(int n, int k){
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j < i and j <= k; j++){
+                dp[i][j] = (0UL + dp[i][j - 1] + dp[i - 1][j]) % 1000000007;
+            }
+            for(int j = i; j <= k; j++){
+                dp[i][j] = (0UL + dp[i][j - 1] + dp[i - 1][j] + 1000000007 - dp[i - 1][j - i]) % 1000000007;
+            }
+        }
+
+        return dp[n][k];
+    }
+};
+
+int main(){
+    Solution *S = new Solution();
+
+    //inputs
+    int n = 3;
+    int k = 1;
+
+    cout << S->kInversePairs(n, k) << endl;
+
+    return 0;
+}
