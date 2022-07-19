e4c243de980855e8f1889643da1caf6750ddfebb
Rick <yenju0425@gmail.com>
2022-07-19 12:45:33 +0800

    20220719

---
+++ Solutions/1074. Number of Submatrices That Sum to Target/CPP/Number_of_Submatrices_That_Sum_to_Target.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution{
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target){
+        int M = matrix.size();
+        int N = matrix[0].size();
+        
+        for(int i = 0; i < M; i++){
+            for(int j = 1; j < N; j++){
+                matrix[i][j] = matrix[i][j] + matrix[i][j - 1];
+            }
+        }
+        
+        int result = 0;
+
+        int sum = 0;
+        unordered_map<int, int> UM;
+        for(int i = 0; i < N; i++){
+            for(int j = i; j < N; j++){
+                sum = 0;
+                UM  = {{0, 1}};
+                for(int k = 0; k < M; k++){
+                    sum     = sum     + matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
+                    result  = result  + UM[sum - target];
+                    UM[sum] = UM[sum] + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main(){
+    Solution *S = new Solution;
+
+    //inputs
+    int target = 0;
+    vector<vector<int>> matrix{
+        {0, 1, 0},
+        {1, 1, 1},
+        {0, 1, 0}
+    };
+
+    cout << S->numSubmatrixSumTarget(matrix, target) << endl;
+
+    return 0;
+}
