675748caf44cc0250e0cc771f09eeda0ce44e690
Rick <63002026+yenju0425@users.noreply.github.com>
2023-02-17 17:47:33 +0000

    20230218 update

---
+++ Solutions/13. Roman to Integer/Ruby/Roman_to_Integer.rb
@@ -0,0 +1,27 @@
+def roman_to_int(s)
+    # create a hash table to store the values of each Roman numeral
+    roman_values = {
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    }
+
+    result = 0
+    length = s.length
+    0.upto(length - 2) { |i|
+        val = roman_values[s[i]]
+        if val < roman_values[s[i + 1]]
+            result -= val
+        else
+            result += val
+        end
+    }
+
+    result + roman_values[s[-1]]
+end
+
+puts roman_to_int("VI")
